30b60f817ebc46e51d15c84010a8f73c60a0a96b
Owen Richards <cabbagedude88@yahoo.co.uk>
2017-12-20 12:46:39 +0000

    First version

---
+++ src/js/views/home/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Home = (props) => {
+    return (
+        <div>
+            <div className="App-header">
+                <h2>Welcome to React</h2>
+            </div>
+            <p className="App-intro">
+                To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+        </div>
+    );
+};
+
+Home.propTypes = {};
+Home.defaultProps = {};
+
+export default Home;
